b65c8ca1048407660480e5fcf349def52adf376a
rolcho <roland@cimem.hu>
2024-06-08 10:45:02 +0200

    Dropping target to 13.0 from 13.1

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Nagy Roland on 13/03/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Nagy Roland on 08/06/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
